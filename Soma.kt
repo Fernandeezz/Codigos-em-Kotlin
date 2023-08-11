d78ea6cc09e817e23ce07a7501f25ddfe2a56927
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2023-08-11 12:54:55 -0300

    Create Soma.kt

---
+++ Soma.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a: Int 
+    var b: Int
+    var r: Int 
+    
+    println("Digite o primeiro número")
+    a = readLine().toString().toInt()
+    println("Digite o segundo número")
+    b = readLine().toString().toInt()
+    r = a + b 
+    println("Soma = $r")  
+}
+
